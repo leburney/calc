acbf935ac74d7f7413c34e4ce56d9aa0fad293b2
leburney <88947913+leburney@users.noreply.github.com>
2021-08-23 02:35:56 +0400

    Added calc

---
+++ calc.py
@@ -0,0 +1,18 @@
+
+op = input("Which operation do you wish to do? [add,minus,mult,div]")
+first = int(input("what is the first number of this operation? "))
+second = int(input("what is the second number of this operation? "))
+if op == "add":
+    re = (first + second)
+    print(re)
+elif op == "minus":
+    re = (first - second)
+    print(re)
+elif op == "multi":
+    re = (first * second)
+    print(re)
+elif op  == "div":
+    re = (first / second)
+    print(re)
+
+print("Result of it is" + " " + str(re))
